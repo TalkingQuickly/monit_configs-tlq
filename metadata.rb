6810041aebc6edbcec60c4d475727c93e08ea5db
Ben Dixon <ben@hillsbede.co.uk>
2013-11-03 19:14:37 +0000

    bump version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "monit_configs-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Monit configs for server components"
-version           "0.0.3"
+version           "0.0.4"
 
 recipe "monit_configs-tlq::memcached", "Monit config for memcached"
 recipe "monit_configs-tlq::mongo", "Monit config for mongodb"
