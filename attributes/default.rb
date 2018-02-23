5dec8ad6cd34484d326973fc5f1ab1bfd2183bff
flyingboy007 <abhilashvr007@gmail.com>
2018-02-23 20:55:08 +0530

    changed postgres version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,2 @@
-default['monit_configs-tlq']['postgres']['pidfile'] = '/var/run/postgresql/9.3-main.pid'
-# for the community cookbook use '/var/lib/mongo/mongod.lock'
-default['monit_configs-tlq']['mongo']['pidfile'] = '/var/run/mongo/mongo.pid'
+default['monit_configs-tlq']['postgres']['pidfile'] = '/var/run/postgresql/9.6-main.pid'
+
